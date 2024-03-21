794368cca79b8c6446e7eedba81747fa6af562c7
Hauke Sommerfeld <development@haukesomm.de>
2024-03-21 23:42:00 +0100

    Clean up CLI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,24 @@ The most recent release is at the top.
 
 ## `0.2.0-snapshot`
 
+### CLI
+
+The following cli options/commands have been changed:
+
+| Status  | Old             | New                     | Note                                                                 |
+|---------|-----------------|-------------------------|----------------------------------------------------------------------|
+| Renamed | `export-assets` | `export`                | Renamed to be shorter                                                |
+| Renamed | `--include`     | `--include-albums`      | Renamed for better clarity                                           |
+| Renamed | `--export`      | `--export-albums`       | Renamed for better clarity                                           |
+| New     |                 | `-H`/`--include-hidden` | Includes hidden assets in the export. <br> _More information below._ |
+
 ### Asset export
 
-- Add new flag `-H`/`--include-hidden` to include hidden assets in the export.  
-By default, hidden assets are not included in the export.
+- Add new feature to include hidden assets in the export. By default, hidden assets are not included.
+
+### General
+
+- Internal code cleanup and refactoring.
 
 ## `0.1.0`
 
--- src/main.rs
+++ src/main.rs
@@ -35,11 +35,11 @@ struct Arguments {
 #[derive(Subcommand, Debug)]
 enum Commands {
 
-    /// Lists all albums in the library
+    /// List all albums in the library
     ListAlbums,
 
-    /// Exports the specified assets from the library to a given location
-    ExportAssets(ExportArgs)
+    /// Export assets from the library to a given location
+    Export(ExportArgs)
 }
 
 #[derive(Args, Debug)]
@@ -48,24 +48,24 @@ struct ExportArgs {
     /// Output directory
     output_dir: String,
 
-    /// Output by album
+    /// Group assets by album
     #[arg(short = 'a', long = "by-album", group = "strategy")]
     album: bool,
 
-    /// Output by year/month
+    /// Group assets by year/month
     #[arg(short = 'm', long = "by-year-month", group = "strategy")]
     year_month: bool,
 
-    /// Output by year/month/album
+    /// Group assets by year/month/album
     #[arg(short = 'M', long = "by-year-month-album", group = "strategy")]
     year_month_album: bool,
 
-    /// Include albums matching the given ids
-    #[arg(short = 'i', long = "include", group = "ids", num_args = 0.., value_delimiter = ' ')]
+    /// Include assets in the albums matching the given ids
+    #[arg(short = 'i', long = "include-albums", group = "ids", num_args = 0.., value_delimiter = ' ')]
     include: Option<Vec<i32>>,
 
-    /// Exclude albums matching the given ids
-    #[arg(short = 'e', long = "exclude", group = "ids", num_args = 1.., value_delimiter = ' ')]
+    /// Exclude assets in the albums matching the given ids
+    #[arg(short = 'e', long = "exclude-albums", group = "ids", num_args = 1.., value_delimiter = ' ')]
     exclude: Option<Vec<i32>>,
 
     /// Include hidden assets
@@ -93,7 +93,7 @@ fn main() {
 
     match args.command {
         Commands::ListAlbums => list_albums(library.db_path()),
-        Commands::ExportAssets(export_args) => export_assets(library, export_args)
+        Commands::Export(export_args) => export_assets(library, export_args)
     }
 }
 

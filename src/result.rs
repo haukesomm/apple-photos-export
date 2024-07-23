616f779e52730073870a86158bf9a8a01755820d
Hauke Sommerfeld <development@haukesomm.de>
2024-07-23 13:49:06 +0200

    Clean up result type

--- src/result.rs
+++ src/result.rs
@@ -13,26 +13,11 @@ impl PhotosExportError {
 pub type PhotosExportResult<T> = Result<T, PhotosExportError>;
 
 
-impl From<String> for PhotosExportError {
-    fn from(message: String) -> Self {
-        PhotosExportError { messages: vec![message] }
-    }
-}
-
-impl From<std::io::Error> for PhotosExportError {
-    fn from(error: std::io::Error) -> Self {
-        PhotosExportError { messages: vec![error.to_string()] }
-    }
-}
-
-impl From<diesel::result::Error> for PhotosExportError {
-    fn from(error: diesel::result::Error) -> Self {
-        PhotosExportError { messages: vec![error.to_string()] }
-    }
-}
-
-impl From<termimad::Error> for PhotosExportError {
-    fn from(error: termimad::Error) -> Self {
+impl<E> From<E> for PhotosExportError
+where
+    E: ToString + Sized,
+{
+    fn from(error: E) -> Self {
         PhotosExportError { messages: vec![error.to_string()] }
     }
 }

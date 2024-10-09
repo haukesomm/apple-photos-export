32608ce2753113a511c9ae653c806ba2366b2979
Hauke Sommerfeld <development@haukesomm.de>
2024-10-09 23:21:08 +0200

    Removes deprecated Chrono call

--- src/foundation/cocoa.rs
+++ src/foundation/cocoa.rs
@@ -1,6 +1,6 @@
 use std::ops::Add;
 
-use chrono::{Local, NaiveDateTime, Offset, TimeDelta};
+use chrono::{DateTime, Local, NaiveDateTime, Offset, TimeDelta};
 use num_traits::cast::FromPrimitive;
 
 /// Delta between the minimal unix and cocoa dates.
@@ -14,11 +14,11 @@ pub fn parse_cocoa_timestamp(cocoa_seconds: f32) -> Result<NaiveDateTime, String
 
     let timestamp_millis = timestamp_secs * 1000;
 
-    let datetime = NaiveDateTime::from_timestamp_millis(timestamp_millis)
+    let datetime = DateTime::from_timestamp_millis(timestamp_millis)
         .ok_or("Could not convert timestamp to NaiveDateTime")?;
 
     let cocoa_unix_delta = TimeDelta::milliseconds(UNIX_COCOA_DELTA_MILLIS);
     let utc_offset = Local::now().offset().fix();
 
-    Ok(datetime.add(cocoa_unix_delta).add(utc_offset))
+    Ok(datetime.add(cocoa_unix_delta).add(utc_offset).naive_local())
 }

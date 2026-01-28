71a977db68ccf59c21bf529cfc0f10ba4d381a38
Hauke Sommerfeld <development@haukesomm.de>
2026-01-28 10:03:11 +0100

    refactor(no_changelog): Use `format!` to build exportable assets query

--- src/db/asset.rs
+++ src/db/asset.rs
@@ -11,6 +11,51 @@ pub fn get_visible_count(conn: &rusqlite::Connection) -> crate::Result<usize> {
     Ok(stmt.query_row([], |row| row.get(0))?)
 }
 
+fn get_exportable_assets_query(album_z_ent_key: i32, asset_z_ent_key: i32) -> String {
+    format!(
+        r"
+        SELECT asset.Z_PK                           AS ID,
+               asset.ZUUID                          AS UUID,
+               asset.ZDIRECTORY                     AS DIR,
+               asset.ZFILENAME                      AS FILENAME,
+               asset.ZUNIFORMTYPEIDENTIFIER         AS UTI,
+               asset.ZDATECREATED                   AS DATETIME,
+               asset.ZHIDDEN                        AS HIDDEN,
+               asset.ZTRASHEDSTATE                  AS TRASHED,
+               asset.ZVISIBILITYSTATE               AS VISIBLE,
+               asset.ZDUPLICATEASSETVISIBILITYSTATE AS DUPLICATE_VISIBILITY,
+               asset.ZADJUSTMENTSSTATE > 0          AS HAS_ADJUSTMENTS,
+               asset_attribs.ZORIGINALFILENAME      AS ORIGINAL_FILENAME,
+               int_res.ZCOMPACTUTI                  AS COMPACT_UTI,
+               GROUP_CONCAT(album.Z_PK, ', ')       AS ALBUM_IDS
+        FROM ZASSET asset
+                 INNER JOIN ZADDITIONALASSETATTRIBUTES asset_attribs
+                            ON asset.Z_PK = asset_attribs.ZASSET
+                 LEFT JOIN ZINTERNALRESOURCE int_res
+                           ON int_res.ZASSET = asset_attribs.ZASSET
+                               AND int_res.ZDATASTORESUBTYPE = 1
+                 LEFT JOIN Z_{}ASSETS album_mapping
+                           ON album_mapping.Z_{}ASSETS = asset.Z_PK
+                 LEFT JOIN ZGENERICALBUM album
+                           ON album_mapping.Z_{}ALBUMS = album.Z_PK
+        WHERE asset.ZTRASHEDSTATE = false
+          AND asset.ZVISIBILITYSTATE = 0
+          AND asset.ZDUPLICATEASSETVISIBILITYSTATE = 0
+          -- Field may not be filled in the database, depending on whether it is an iCloud-enabled or
+          -- offline library
+          AND (int_res.ZLOCALAVAILABILITY = 1 OR int_res.ZLOCALAVAILABILITY IS NULL)
+          -- Album kind values:
+          -- - 3999: Root album
+          -- - 4000: User-created folder
+          -- - 2: User-created album
+          AND (album.ZKIND IS NULL OR (album.ZTRASHEDSTATE = false AND album.ZKIND IN (3999, 4000, 2)))
+        GROUP BY asset.Z_PK
+        ORDER BY asset.Z_PK
+        ",
+        album_z_ent_key, asset_z_ent_key, album_z_ent_key
+    )
+}
+
 /// Get the count of all assets in the database that are _exportable_, meaning they are not
 /// part of the "hidden" album or moved to the trash, and are locally available in the library
 /// file.
@@ -24,10 +69,7 @@ pub fn get_exportable_assets(conn: &rusqlite::Connection) -> crate::Result<Vec<A
     let album_z_ent: i32 = conn.query_row(album_z_ent_sql, [], |row| row.get(0))?;
 
     // Load the SQL template and replace the placeholders with actual Z_ENT values
-    let raw_sql_template = include_str!("../../queries/get_exportable_assets.sql");
-    let raw_sql = raw_sql_template
-        .replace("_ALBUM_Z_ENT_", &album_z_ent.to_string())
-        .replace("_ASSET_Z_ENT_", &asset_z_ent.to_string());
+    let raw_sql = get_exportable_assets_query(album_z_ent, asset_z_ent);
 
     let mut stmt = conn.prepare(&raw_sql)?;
 
